bf214bf795be61fc485538d2c17edcad5798789a
Shawn Xiao (YuC) <agxiao@qq.com>
2019-11-27 09:25:04 +0000

    Player Test

---
+++ PlayerTests/DieTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Monopoly;
+
+
+namespace MonopolyTests
+{
+    [TestFixture]
+    public class DieTest
+    {
+        RollingDie rollingDie;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rollingDie = new RollingDie();
+        }
+
+        [Test]
+        public void CheckSidesCount_a([Range(6, 10, 1)] int dieSize)
+        {
+            //arrange
+
+
+            //act
+            rollingDie.SetDieSideCount(dieSize);
+
+            //assert
+            Assert.That(rollingDie.sidesCount, Is.EqualTo(dieSize));
+        }
+
+        [Test]
+        public void CheckOutcomeIsRandomNumber([Range(1, 100, 1)] int tryTimes)
+        {
+
+            //arrange
+            rollingDie.sidesCount = 6; //rollingDie is the class 
+            int numberGet;
+
+            //Act
+            numberGet = rollingDie.Roll(); //put the value of the method into the numberGet variable
+
+            //Assert
+
+            Assert.That(numberGet, Is.TypeOf<int>());
+        }
+    }
+}
+
---
+++ PlayerTests/PlayerTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Monopoly;
+
+namespace MonopolyTests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [TestCase("Test Name")]
+        public void WhenPlayerIsInitialised_PlayerNameIsSet(string name)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetPlayerName, Is.EqualTo(name));
+            #endregion
+
+        }
+
+        [TestCase("Test Name")]
+        public void WhenPlayerIsInitialised_PlayerStatusIsSet_ToAlive(string name)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetPlayerStatus, Is.EqualTo(true));
+            #endregion
+
+        }
+
+        [TestCase("Test Name")]
+        public void WhenPlayerIsInitialised_PlayerBalanceIsSet_To1000(string name)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetBalance, Is.EqualTo(1000));
+            #endregion
+
+        }
+
+        [TestCase("Test Name", 100)]
+        [TestCase("Test Name", -100)]
+        public void PlayersBalanceIsChanged_WhenChangeBalanceIsCalledWithAValue(string name, int money)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            player.ChangeBalance(money);
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetBalance, Is.EqualTo(1000 + money));
+            #endregion
+
+        }
+
+        [TestCase("Test Name")]
+        public void PlayerStatusIsSet_ToNotAlive_WhenRemovePlayerIsCalled(string name)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            player.RemovePlayer();
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetPlayerStatus, Is.EqualTo(false));
+            #endregion
+
+        }
+        [TestCase("Test Name")]
+        public void PlayerIDIsSet_ToNotAlive_WhenRemovePlayerIsCalled(string name)
+        {
+            #region Arrange
+            Player player = new Player(name);
+            #endregion
+
+            #region Act
+            player.RemovePlayer();
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetPlayerStatus, Is.EqualTo(false));
+            #endregion
+
+        }
+
+        [TestCase("Test Name" , 1)]
+        public void WhenPlayerIsInitialised_PlayerNameIsSet(string name, int id)
+        {
+            #region Arrange
+            Player player = new Player(name, id);
+            #endregion
+
+            #region Act
+            #endregion
+
+            #region Assert
+            Assert.That(player.GetPlayerID, Is.EqualTo(id));
+            #endregion
+
+        }
+
+    }
+}
